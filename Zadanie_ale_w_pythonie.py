de3de7db11270557c77a7eba70647e3f724cf3e6
yedu <kacper.wiecek@student.wsb.edu.pl>
2025-04-21 14:51:12 +0200

    dodanie kalkulatora z dwoma liczbami

--- Zadanie_ale_w_pythonie.py
+++ Zadanie_ale_w_pythonie.py
@@ -1,1 +1,25 @@
+def kalkulator():
+    try: 
+        a = float(input("Podaj pierwsza liczbe: "))
+        b = float(input("Podaj druga liczbe: "))
+        operacja = input("Wybierz dana operacje:  (+, -, *, /):")
 
+        if operacja == "+":
+            wynik = a + b
+        elif operacja == "-":
+            wynik = a - b
+        elif operacja == "*":
+            wynik = a * b
+        elif operacja == "/":
+            if b == 0:
+                print ("Uwaga! Nie mozna dzielic przez zero!")
+                return
+            wynik = a / b
+        else: 
+            print("Nieznana operacja")
+            return
+        
+        print (f"wynik: {wynik}")
+    except ValuveError: 
+        print (" Uwaga! Podano nieprawidlowa wartosc.")
+kalkulator ()
