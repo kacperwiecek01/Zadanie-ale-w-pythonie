2f81ec7b6f147d5169f8996558c9fb0a1237752a
yedu <kacper.wiecek@student.wsb.edu.pl>
2025-04-21 15:18:57 +0200

    Dodane konwersje temperatur oraz menu wybrania opcji

--- Zadanie_ale_w_pythonie.py
+++ Zadanie_ale_w_pythonie.py
@@ -20,6 +20,41 @@ def kalkulator():
             return
         
         print (f"wynik: {wynik}")
-    except ValuveError: 
+    except ValueError: 
         print (" Uwaga! Podano nieprawidlowa wartosc.")
-kalkulator ()
+
+def konwersja_temperatur():
+
+    wybor = input ("Wybierz kierunek oknwersji (C- Celsujsz -> Fahrenheit, F - Farhenheit -> Celsjusz): ")
+
+    if wybor == "C":
+        celsjusz = float(input ("Podaj temperature w C:"))
+        Farheneit = celsjusz * 1.8 + 32
+        print (f"{celsjusz}C = {Farheneit:.2f}F")
+    elif wybor == "F":
+        Farheneit = float(input("Podaj temperature w F: "))
+        celsjusz = (Farheneit - 32) / 1.8
+        print(f"{Farheneit}F = {celsjusz:.2f}C")
+    else:
+        print("Nieprawidlowy wybor. Wybierz C lub F.")
+konwersja_temperatur()
+
+def main ():
+    while True:
+        print("\n--Menu--")
+        print("1. Kalkulator")
+        print("2. Konwersja temperatury")
+        print("0. Wyjscie")
+
+        choice = input("Wybierz zadanie: ")
+
+        if choice == "1":
+            kalkulator()
+        elif choice == "2":
+            konwersja_temperatur()
+        elif choice == "0":
+            print ("zamykanie programu...")
+            break
+        else: 
+            print("Nieprawidlowy wybor opcji")
+main()
